351c19673378041f3364ca8987ae4b3989bea4a3
Prateek Gogia <pgogia@amazon.com>
2022-02-28 17:51:04 -0600

    introduce substratet

---
+++ substrate/cmd/cmd.go
@@ -0,0 +1,11 @@
+package cmd
+
+import (
+	"fmt"
+
+	_ "github.com/aws/aws-sdk-go/aws/awserr"
+)
+
+func Run() {
+	fmt.Println("running import-go-modules in substrate")
+}
---
+++ substrate/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/prateekgogia/importing-go-modules/substrate/cmd"
+)
+
+func main() {
+	cmd.Run()
+}
