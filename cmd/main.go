2034ee75dff2b18ea6208019dc5f5c24d506303f
Prateek Gogia <pgogia@amazon.com>
2022-02-28 17:40:27 -0600

    running commands

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/aws/aws-sdk-go/aws/awserr"
+)
+
+func main() {
+	fmt.Println("running import-go-modules")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/prateekgogia/importing-go-modules
+
+go 1.17
+
+require github.com/aws/aws-sdk-go v1.43.8
