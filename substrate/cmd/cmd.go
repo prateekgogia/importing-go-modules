f0f15a7c3f6e81d136ab282d45723bc790700ce4
Prateek Gogia <pgogia@amazon.com>
2022-03-11 18:03:07 -0600

    make it main

--- substrate/cmd/cmd.go
+++ substrate/cmd/cmd.go
@@ -1,10 +1,14 @@
-package cmd
+package main
 
 import (
 	"fmt"
 
 	_ "github.com/aws/aws-sdk-go/aws/awserr"
 )
+
+func main() {
+	Run("main")
+}
 
 func Run(v string) {
 	fmt.Println("running import-go-modules in ", v)
